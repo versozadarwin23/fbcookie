d5b25d7a4f6b4eb70a1f056c363984af3b505012
Dars <109713017+versozadarwin23@users.noreply.github.com>
2026-02-20 22:38:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ from datetime import datetime
 from playwright.sync_api import sync_playwright, TimeoutError as PlaywrightTimeoutError
 
 __version__ = "2"
-UPDATE_URL = "https://raw.githubusercontent.com/versozadarwin23/autopost/refs/heads/main/main.py"
-VERSION_CHECK_URL = "https://raw.githubusercontent.com/versozadarwin23/autopost/refs/heads/main/version.txt"
+UPDATE_URL = "https://raw.githubusercontent.com/versozadarwin23/fbcookie/refs/heads/main/main.py"
+VERSION_CHECK_URL = "https://raw.githubusercontent.com/versozadarwin23/fbcookie/refs/heads/main/version.txt"
 
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("dark-blue")
